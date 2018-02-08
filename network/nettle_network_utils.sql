91a313c9e906cd4f819abe054a79b410b29e9a66
Roberto Reale <rober.reale@gmail.com>
2018-02-08 16:04:17 +0100

    Fix bug in INT_TO_IP method.

--- network/nettle_network_utils.sql
+++ network/nettle_network_utils.sql
@@ -132,7 +132,7 @@ AS
 		v8 VARCHAR2(8);
 	BEGIN
 		-- 1. convert the integer into hexadecimal representation
-		v8 := TO_CHAR(ip_address, 'FMXXXXXXXX');
+		v8 := TO_CHAR(ip_address, 'FM0000000X');
 		-- 2. convert each XX portion back into decimal
 		RETURN to_number(substr(v8,1,2),'XX')
 			|| '.' || to_number(substr(v8,3,2),'XX')
