58bf3275608a218c8a6195d34269deaba521b7ed
Roberto Reale <rober.reale@gmail.com>
2018-02-14 17:38:51 +0100

    Fix utPLSQL installation process.

--- .travis/utplsql/install.sh
+++ .travis/utplsql/install.sh
@@ -2,4 +2,4 @@
 # vim: set et sw=2 ts=2:
 
 cd utPLSQL/source
-$ORACLE_HOME/bin/sqlplus / as sysdba @@install_headless.sql $UTPLSQL_OWNER $UTPLSQL_PASSWORD $UTPLSQL_TABLESPACE
+$ORACLE_HOME/bin/sqlplus / as sysdba @install_headless.sql $UTPLSQL_OWNER $UTPLSQL_PASSWORD $UTPLSQL_TABLESPACE
