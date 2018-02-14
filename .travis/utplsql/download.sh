f33212272aa7a8b78b4614852b4c0c37e35f0c4a
Roberto Reale <rober.reale@gmail.com>
2018-02-14 17:31:17 +0100

    Fix utPLSQL installation process.

--- .travis/utplsql/download.sh
+++ .travis/utplsql/download.sh
@@ -1,15 +1,15 @@
 #!/bin/sh -e
 # vim: set et sw=2 ts=2:
 
-# Get the url to latest release "zip" file
+# Get the url to latest release tarball
 UTPLSQL_GITHUB_API=https://api.github.com/repos/utPLSQL/utPLSQL/releases/latest
 UTPLSQL_DOWNLOAD_URL=$(
     curl --silent $UTPLSQL_GITHUB_API          \
   | awk '/browser_download_url/ { print $2 }'  \
-  | grep ".zip" | sed 's/"//g')
+  | tr -d \" | grep .tar.gz$)
 
-# Download the latest release "zip" file
-curl -Lk "${UTPLSQL_DOWNLOAD_URL}" -o utPLSQL.zip
+# Download the latest release tarball
+curl -Lks "${UTPLSQL_DOWNLOAD_URL}" -o utPLSQL.tar.gz
 
-# Extract downloaded "zip" file
-unzip -q utPLSQL.zip
+# Extract downloaded tarball
+tar xzf utPLSQL.tar.gz
